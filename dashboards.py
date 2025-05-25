927f1c66959d3e879d0af26b4af03d4a9e41a4e9
Emerson Reinaldo <83143982+EmersonReinaldo95@users.noreply.github.com>
2025-05-25 12:41:43 -0400

    Update dashboards.py

--- dashboards.py
+++ dashboards.py
@@ -185,8 +185,7 @@ selected_years = st.sidebar.multiselect("📅 Selecione os Anos:", options=all_y
 movimento_options = sorted(material_df['Descricao Movimento'].dropna().unique())
 default_movimento_str = {movimento_options[6]}
 default_movimento_index = 6
-if default_movimento_str in movimento_options: default_movimento_index = movimento_options.index(default_movimento_str)
-elif movimento_options: st.sidebar.warning(f"Tipo de movimento '{default_movimento_str}' encontrado. Usando '{movimento_options[6]}' como padrão.")
+
 selected_movimento_consumo = st.sidebar.selectbox("📉 Tipo de Movimento para Consumo:", options=movimento_options, index=default_movimento_index if movimento_options else 0)
 pdf_download_button_placeholder = st.sidebar.empty()
 
